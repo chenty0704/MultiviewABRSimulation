383087e5bb02927afb2e796ea623d938c62ecd4a
Kevin Chen <tianyuchen@umass.edu>
2025-01-23 12:36:23 -0500

    Added throughput-based controller.

---
+++ tests/MultiviewABRControllers/ThroughputBasedControllerTest.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+
+import System.Base;
+
+import MultiviewABRSimulation.Base;
+import MultiviewABRSimulation.MultiviewABRControllers.IMultiviewABRController;
+import MultiviewABRSimulation.MultiviewABRControllers.ThroughputBasedController;
+import MultiviewABRSimulation.ViewPredictors.StaticPredictor;
+
+using namespace std;
+
+TEST(ThroughputBasedControllerTest, BasicControl) {
+    const StreamingConfig streamingConfig = {1., {1., 2., 4., 8.}, 4, 0.75, 0.083, 5.};
+    StaticPredictor predictor({4, 1., 2.});
+    const ThroughputBasedController controller({streamingConfig, predictor});
+    MultiviewABRControllerContext context;
+
+    context.ThroughputMbps = 2.;
+    EXPECT_EQ(controller.GetControlAction(context).BitrateIDs, vector({0, 0, 0, 0}));
+
+    context.ThroughputMbps = 15.;
+    EXPECT_EQ(controller.GetControlAction(context).BitrateIDs, vector({3, 0, 0, 0}));
+}
